a8f6d1341d9400715cb6aec6f6c92108d658af5d
root <root@DESKTOP-OERP6A6.localdomain>
2022-10-08 22:29:06 -0400

    update for testing

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 4000;
+const test = require("./router/test");
+
+app.use("/api", test);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
