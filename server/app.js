3e1f04f5de8947614bef847ae457deb67dbe7bb9
songcynt <cynthia.song@mail.utoronto.ca>
2022-10-09 16:08:23 -0400

    add some placeholder text

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const nothing = require("./router/nothing");
 app.use("/api", nothing);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send("I am running! I'm gonna talk to a database!")
 })
 
 app.listen(port, () => {
