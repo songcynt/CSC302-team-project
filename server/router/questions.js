8decbc7885f5cc025b7e7e108e75e65f2b240c8f
bhatiate <tevan.bhatia@mail.utoronto.ca>
2022-10-31 18:49:27 -0400

    Answered question 1 with array result and fixed DB connection pathway

--- server/router/questions.js
+++ server/router/questions.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const sqlite3 = require("sqlite3").verbose();
 const fs = require("fs");
-fs.stat("../data/source.db", (err, stats) => {
+fs.stat("data/source.db", (err, stats) => {
   //Make sure the database exists
   if (err) {
     console.log(err);
@@ -12,7 +12,7 @@ fs.stat("../data/source.db", (err, stats) => {
     console.log(stats);
   }
   const db = new sqlite3.Database(
-    "../data/source.db",
+    "data/source.db",
     sqlite3.OPEN_READWRITE,
     (err) => {
       if (err) {
@@ -22,11 +22,26 @@ fs.stat("../data/source.db", (err, stats) => {
     }
   );
   router.get("/question1", (req, res) => {
-    db.all("SELECT * FROM Employee_Compensation_SF", (err, rows) => {
+    db.all("SELECT Salaries, Department FROM Employee_Compensation_SF", (err, rows) => {
       if (err) {
         console.error(err.message);
       }
-      res.send(rows);
+      let n = rows.length;
+      let info = {};
+      for(let i = 0; i < n; i++){
+        if(rows[i].Department in info){
+          info[rows[i].Department].push(rows[i].Salaries);
+        }else{
+          info[rows[i].Department] = [rows[i].Salaries];
+        }
+      }
+      let salaries = [];
+      let departments = [];
+      for(let key in info){
+        salaries.push((info[key].reduce((a, b) => a + b, 0) / info[key].length).toFixed(2));
+        departments.push(key);
+      }
+      res.send({salaries, departments});
     });
   });
 });
