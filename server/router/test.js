52a1c80f23cbee4e277b2a450b09f3a44695ee82
Sung Hyun Cho <roy.cho@mail.utoronto.ca>
2022-11-02 04:15:26 -0400

    add new test

--- server/router/test.js
+++ server/router/test.js
@@ -1,3 +1,11 @@
-test('this is sample test', () => {
-   console.log("I am running lots of tests!"); 
+const app = require("../app.js")
+const supertest = require("supertest")
+const requestWithSupertest = supertest(app);
+
+describe('Testing Routes', function () {
+
+  it('responds to /', async function() {
+    const res = await requestWithSupertest.get('/');
+    expect(res.status).toEqual(200);
+  });
 });
